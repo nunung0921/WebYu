ecb24ecaaa541776488d4e13e53176ba4163b660
nunung0921 <nunung0921@gmail.com>
2024-05-12 02:28:42 +0800

    old dashboard

--- dashboard_sidebar_start.php
+++ dashboard_sidebar_start.php
@@ -50,7 +50,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="admn_dashboard.php">
+                <a class="nav-link" href="admin_dashboard.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
