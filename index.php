9ff54571f0178fe12a61898fb000f0db372400d6
nunung0921 <nunung0921@gmail.com>
2024-05-15 23:31:24 +0800

    opacity

--- index.php
+++ index.php
@@ -1111,7 +1111,7 @@ nav {
     .navbar {
         display: flex;
         justify-content: center; /* Center the navbar items horizontally */
-        background-color: rgba(0, 123, 255, 0.9);
+        background-color: rgba(0, 123, 255, 0.7);
     }
     .dropdown-menu {
         top: 100% !important; /* Ensure dropdown menu appears below the dropdown toggle */
