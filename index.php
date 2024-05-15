6f6715ebc87abb6f94bdda195b134cd0a3213a33
nunung0921 <nunung0921@gmail.com>
2024-05-16 01:54:51 +0800

    live

--- index.php
+++ index.php
@@ -1632,7 +1632,7 @@ nav {
      
      
       <section class="weather__container weather_container" id="weather">
-      <center><h2 class="section__header"><br><br><br>Location</h2></center>
+      <center><h2 class="section__header"><br><br><br>Weather Forecast</h2></center>
       <a class="weatherwidget-io" href="https://forecast7.com/en/15d67120d73/guimba/" data-label_1="GUIMBA" data-label_2="WEATHER" data-theme="original" >GUIMBA WEATHER</a>
 <script>
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
