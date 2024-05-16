abe4b491db8432a7434090a0d9f658d5f4c18906
nunung0921 <nunung0921@gmail.com>
2024-05-16 22:21:03 +0800

    showing button

--- dashboard_sidebar_start.php
+++ dashboard_sidebar_start.php
@@ -35,7 +35,7 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        < class="navbar-nav bg-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="admin_dashboard.php">
@@ -181,12 +181,12 @@
 
 
             <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
+            <!--<hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Sidebar Toggler (Sidebar) -->
+             Sidebar Toggler (Sidebar) 
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
+            </div>-->
 
         </ul>
         <!-- End of Sidebar -->
