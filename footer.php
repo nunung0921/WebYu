452042439bf5a0ae347c901fc12a839465b325fe
nunung0921 <nunung0921@gmail.com>
2024-05-17 22:44:22 +0800

    footer

---
+++ footer.php
@@ -0,0 +1,61 @@
+<footer id="footer" class="bg-primary text-white d-flex-column text-center">
+            <hr class="mt-0">
+
+            
+                    <!--/.Third column-->
+
+                    <hr class="clearfix w-100 d-md-none mb-0">
+ 
+                    <!--Fourth column-->
+
+                    <div class="col-md-3 mx-auto shfooter" id="down">
+                        <h5 class="my-2 font-weight-bold d-none d-md-block">Contact Us:</h5>
+                        <div class="d-md-none title" data-target="#Contact-Us" data-toggle="collapse">
+                        <div class="mt-3 font-weight-bold">Contact Us:
+                            <div class="float-right navbar-toggler">
+                            <i class="fas fa-angle-down"></i>
+                            <i class="fas fa-angle-up"></i>
+                            </div>
+                        </div>
+                        </div>
+                        <ul class="list-unstyled collapse" id="Contact-Us">
+                            <li>
+                                <div class="zoom">
+                                    <div class="chip" style="font-size:10px;">
+                                            <img src="assets/goloo.png" alt="Person" width="50" height="50">
+                                        Barangay Biclatan | (046) 509 1644
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+             <!--/.Footer Links-->
+
+            <hr class="mb-0">
+
+            <!--Copyright-->
+
+            <div class="py-3 text-center">
+                Copyright 2023 -
+                <script>
+                document.write(new Date().getFullYear())
+                </script> 
+                  | Barangay Biclatan Information System
+            </div>
+
+        </footer>
+        <style>
+    html, body {
+        height: 100%;
+    }
+    #content-wrapper {
+        flex: 1;
+    }
+    #footer {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+</style>
