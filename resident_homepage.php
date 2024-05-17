c57804b2c983bb90d0aab4f7889ebda9f647bdcb
nunung0921 <nunung0921@gmail.com>
2024-05-17 22:21:13 +0800

    margin

--- resident_homepage.php
+++ resident_homepage.php
@@ -330,7 +330,7 @@
         <div class="logo">
             <a href="#"><img src="icons/yuson1.png" alt="logo" height="60px" /></a>
         </div>
-        <a class="navbar-brand" href="resident_homepage.php"><b> Webyu</b></a>
+        <a class="navbar-brand" href="resident_homepage.php" style="margin-left: 5px;"><b>Webyu</b></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
