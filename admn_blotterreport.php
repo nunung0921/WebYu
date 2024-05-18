c27d588d455f451506f75aaeaf393427f3155764
nunung0921 <nunung0921@gmail.com>
2024-05-18 23:23:18 +0800

    walkin

--- admn_blotterreport.php
+++ admn_blotterreport.php
@@ -67,63 +67,6 @@
     </div>
 
     <br>
-    
-?>
-
-<?php 
-    include('dashboard_sidebar_start.php');
-?>
-
-<style>
-    .input-icons i {
-        position: absolute;
-    }
-        
-    .input-icons {
-        width: 30%;
-        margin-bottom: 10px;
-        margin-left: 34%;
-    }
-        
-    .icon {
-        padding: 10px;
-        min-width: 40px;
-    }
-    .form-control{
-        text-align: center;
-    }
-</style>
-
-<!-- Begin Page Content -->
-
-<div class="container-fluid">
-
-    <!-- Page Heading -->
-
-    <div class="row"> 
-        <div class="col text-center"> 
-            <h1> Peace and Order Report Data</h1>
-        </div>
-    </div>
-
-    <hr>
-    <br><br>
-
-    <div class="row"> 
-        <div class="col">
-            <form method="POST">
-                <div class="input-icons">
-                    <i class="fa fa-search icon"></i>
-                    <input type="search" class="form-control" name="keyword" style="border-radius: 30px;" value="" required=""/>
-                </div>
-                <button class="btn btn-success" style="width: 90px; font-size: 18px; border-radius:30px; margin-left:41.5%;" name="search_bspermit">Search</button>
-                <a href="admn_blotterreport.php" class="btn btn-info" style="width: 90px; font-size: 18px; border-radius:30px;">Reload</a>
-            </form>
-            <br>
-        </div>
-    </div>
-
-    <br>
     <button class="btn btn-success" style="width: 95px; height: 40px; font-size: 14px; border-radius:5px; margin-bottom: 5px;" data-toggle="modal" data-target="#exampleModalCenter"><i class="fas fa-plus icon" style="padding-left: 0; padding-top: 0; padding-bottom: 0;"></i>Add</button>
     <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
