5c54b0b7924182aa3215e84cb7eeac0c118256ac
nunung0921 <nunung0921@gmail.com>
2024-05-18 23:39:52 +0800

    re

--- resident_changepass.php
+++ resident_changepass.php
@@ -414,10 +414,7 @@
 
         <!-- Back-to-Top and Back Button -->
 
-        <a data-toggle="tooltip" title="Back-To-Top" class="top-link hide" href="" id="js-top">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6"><path d="M12 6H0l6-6z"/></svg>
-            <span class="screen-reader-text">Back to top</span>
-        </a>
+       
 
         <!-- Eto yung navbar -->
 
