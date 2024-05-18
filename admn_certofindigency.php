45249388da762baf9ccfd8f305c626255c732e5d
nunung0921 <nunung0921@gmail.com>
2024-05-19 01:52:40 +0800

    design

--- admn_certofindigency.php
+++ admn_certofindigency.php
@@ -13,7 +13,7 @@
    
 ?>
 
-<?php 
+<for?php 
     include('dashboard_sidebar_start.php');
 ?>
 <style>
@@ -66,7 +66,7 @@
 
     <br>
     <button class="btn btn-success" style="width: 95px; height: 40px; font-size: 14px; border-radius:5px; margin-bottom: 5px;" data-toggle="modal" data-target="#exampleModalCenter"><i class="fas fa-plus icon" style="padding-left: 0; padding-top: 0; padding-bottom: 0;"></i>Add</button>
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<s="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -182,9 +182,6 @@
                                     </div>
 
                                 </div>
-                                
-                            
-                        </div>
 
                 
                 
@@ -200,6 +197,7 @@
                         </div> 
                     </div>
                 </div>
+</form>
             </div>
     <div class="row"> 
         <div class="col"> 
@@ -208,7 +206,6 @@
             ?>
         </div>
     </div>
-</div>
 </div>
 <!-- End of Main Content -->
 
