6b63990120a2f3bb00656c200c7fc976c2edea0f
nunung0921 <nunung0921@gmail.com>
2024-05-19 22:33:07 +0800

    email config

--- send_otp.php
+++ send_otp.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'path/to/your/database/connection.php';
+include 'conn.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
@@ -13,7 +13,7 @@ if (isset($data['email'])) {
     // You can use mail() function or a library like PHPMailer to send the OTP
     $subject = "Your OTP Code";
     $message = "Your OTP code is $otp";
-    $headers = "From: your-email@example.com";
+    $headers = "From: rafaeltosper@gmail.com";
 
     if (mail($email, $subject, $message, $headers)) {
         echo json_encode(['success' => true]);
