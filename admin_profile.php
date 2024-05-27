a2daeeefc6d08b1647a259fb099c940272e100af
marjun111 <166087018+marjun111@users.noreply.github.com>
2024-05-27 23:39:49 +0800

    Update admin_profile.php

--- admin_profile.php
+++ admin_profile.php
@@ -1,1 +1,447 @@
 
+<?php 
+    error_reporting(E_ALL ^ E_WARNING);
+    require('classes/resident.class.php');
+    ini_set('display_errors',1);
+    $userdetails = $residentbmis->get_userdata();
+    $id_admin = $_GET['id_admin'];
+    $admin = $residentbmis->get_single_admin($id_admin);
+    
+        
+
+    $residentbmis->profile_update_admin();
+
+?>
+
+<!DOCTYPE html> 
+<html>
+
+    <head> 
+       <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Barangay Biclatan Information System</title>
+
+        <!-- Custom fonts for this template-->
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+            rel="stylesheet">
+        
+        <!-- Custom styles for this template-->
+        <link href="css/sb-admin-2.min.css" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/67a9b7069e.js" crossorigin="anonymous"></script>
+
+
+
+    <style>
+
+        /* Back-to-Top */
+
+        .top-link {
+        transition: all 0.25s ease-in-out;
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        display: inline-flex;
+        cursor: pointer;
+        align-items: center;
+        justify-content: center;
+        margin: 0 3em 3em 0;
+        border-radius: 50%;
+        padding: 0.25em;
+        width: 80px;
+        height: 80px;
+        background-color: #3661D5;
+        }
+        .top-link.show {
+        visibility: visible;
+        opacity: 1;
+        }
+        .top-link.hide {
+        visibility: hidden;
+        opacity: 0;
+        }
+        .top-link svg {
+        fill: white;
+        width: 24px;
+        height: 12px;
+        }
+        .top-link:hover {
+        background-color: #3498DB;
+        }
+        .top-link:hover svg {
+        fill: #000000;
+        }
+
+        .screen-reader-text {
+        position: absolute;
+        clip-path: inset(50%);
+        margin: -1px;
+        border: 0;
+        padding: 0;
+        width: 1px;
+        height: 1px;
+        overflow: hidden;
+        word-wrap: normal !important;
+        clip: rect(1px, 1px, 1px, 1px);
+        }
+        .screen-reader-text:focus {
+        display: block;
+        top: 5px;
+        left: 5px;
+        z-index: 100000;
+        clip-path: none;
+        background-color: #eee;
+        padding: 15px 23px 14px;
+        width: auto;
+        height: auto;
+        text-decoration: none;
+        line-height: normal;
+        color: #444;
+        font-size: 1em;
+        clip: auto !important;
+        }
+
+        /* Navbar Buttons */
+
+        .btn3 {
+        border-radius: 20px;
+        border: none; /* Remove borders */
+        color: white; /* White text */
+        font-size: 16px; /* Set a font size */
+        cursor: pointer; /* Mouse pointer on hover */
+        margin-left: 15%;
+        padding: 8px 22px;
+        }
+
+        .btn4 {
+        border-radius: 20px;
+        border: none; /* Remove borders */
+        color: white; /* White text */
+        font-size: 16px; /* Set a font size */
+        cursor: pointer; /* Mouse pointer on hover */
+        padding: 8px 22px;
+        margin-left: .1%;
+        }
+
+        .btn5 {
+        border-radius: 20px;
+        border: none; /* Remove borders */
+        color: white; /* White text */
+        font-size: 16px; /* Set a font size */
+        cursor: pointer; /* Mouse pointer on hover */
+        margin-left: .1%;
+        padding: 8px 22px;
+        }
+
+        .btn6 {
+        border-radius: 20px;
+        border: none; /* Remove borders */
+        color: white; /* White text */
+        font-size: 16px; /* Set a font size */
+        cursor: pointer; /* Mouse pointer on hover */
+        padding: 8px 22px;
+        margin-left: .1%;
+        }
+
+
+        /* Darker background on mouse-over */
+
+        .btn3:hover {
+        background-color: RoyalBlue;
+        color: black;
+        }
+
+        .btn4:hover {
+        background-color: RoyalBlue;
+        color: black;
+        }
+
+        .btn5:hover {
+        background-color: RoyalBlue;
+        color: black;
+        }
+
+        .btn6:hover {
+        background-color: RoyalBlue;
+        color: black;
+        }
+
+        .footerlinks{
+        color:white;
+        }
+        .shfooter .collapse {
+            display: inherit;
+        }
+            @media (max-width:767px) {
+        .shfooter ul {
+                margin-bottom: 0;
+        }
+
+        .shfooter .collapse {
+                display: none;
+        }
+
+        .shfooter .collapse.show {
+                display: block;
+        }
+
+        .shfooter .title .fa-angle-up,
+        .shfooter .title[aria-expanded=true] .fa-angle-down {
+                display: none;
+        }
+
+        .shfooter .title[aria-expanded=true] .fa-angle-up {
+                display: block;
+        }
+
+        .shfooter .navbar-toggler {
+                display: inline-block;
+                padding: 0;
+        }
+
+        }
+
+        .resize {
+            text-align: center;
+        }
+        .resize {
+            margin-top: 3rem;
+            font-size: 1.25rem;
+        }
+        /*RESIZESCREEN ANIMATION*/
+        .fa-angle-double-right {
+            animation: rightanime 1s linear infinite;
+        }
+
+        .fa-angle-double-left {
+            animation: leftanime 1s linear infinite;
+        }
+        @keyframes rightanime {
+            50% {
+                transform: translateX(10px);
+                opacity: 0.5;
+        }
+            100% {
+                transform: translateX(10px);
+                opacity: 0;
+        }
+        }
+        @keyframes leftanime {
+            50% {
+                transform: translateX(-10px);
+                opacity: 0.5;
+        }
+            100% {
+                transform: translateX(-10px);
+                opacity: 0;
+        }
+        }
+
+        /* Contact Chip */
+
+        .chip {
+        display: inline-block;
+        padding: 0 25px;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 25px;
+        background-color: #2C54C1;
+        margin-top: 5px;
+        }
+
+        .chip img {
+        float: left;
+        margin: 0 10px 0 -25px;
+        height: 50px;
+        width: 50px;
+        border-radius: 50%;
+        }
+
+        .zoom {
+        transition: transform .3s;
+        }
+
+        .zoom:hover {
+        -ms-transform: scale(1.4); /* IE 9 */
+        -webkit-transform: scale(1.4); /* Safari 3-8 */
+        transform: scale(1.4); 
+        }
+
+    </style>
+    <body> 
+        <?php
+            include('dashboard_sidebar_start.php');
+            ?>
+
+        <!-- Back-to-Top and Back Button -->
+
+        <a data-toggle="tooltip" title="Back-To-Top" class="top-link hide" href="" id="js-top">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 6"><path d="M12 6H0l6-6z"/></svg>
+            <span class="screen-reader-text">Back to top</span>
+        </a>
+
+        <!-- Eto yung navbar -->
+
+        
+
+        <div id="down2"></div>
+
+        <br>
+
+        <div class="container"> 
+            <div class="card" style="margin-top: 2em;">  
+                    <div class="card-header bg-primary text-white" style="font-size:20px"> Personal Information </div>
+                <div class="card-body"> 
+                    <form method="post">
+
+                        <h6>
+                            <i class="fas fa-user-edit"></i>
+                            Update Information
+                        </h6>
+
+                        <hr>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label>Last Name:</label>
+                                    <input class="form-control" name="lname" value="<?= $admin['lname'];?>" required>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label>First Name:</label>
+                                    <input class="form-control" name="fname" value="<?= $admin['fname'];?>" required>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <label>Middle Name:</label>
+                                    <input class="form-control" name="mi" value="<?= $admin['mi'];?>">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label>Email:</label>
+                                    <input class="form-control" name="email" value="<?= $admin['email'];?>" required>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+
+                        
+
+                        
+
+                        <div class="row" style="margin-bottom: 5px;"> 
+                            <div class="col-xl-12">
+                                <div class="form-inline">
+                                    <button class="btn btn-primary" style="margin-left: .2%; width:143px;" type="submit" name="profile_update_admin"> Update </button>
+                                
+                                        <a href="admin_dashboard.php" class="btn btn-danger" style="margin-left: .2%; width:143px;" type="submit"> Cancel </a>
+                                    
+                                    <a href="admin_profile.php?id_admin=<?= $userdetails['id_admin'];?>"></a>   
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>                               
+            </div>
+        </div>
+
+        <br>
+        <br>
+        <br>
+
+
+<!-- End of Main Content -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-modal/2.2.6/js/bootstrap-modalmanager.min.js" integrity="sha512-/HL24m2nmyI2+ccX+dSHphAHqLw60Oj5sK8jf59VWtFWZi9vx7jzoxbZmcBeeTeCUc7z1mTs3LfyXGuBU32t+w==" crossorigin="anonymous"></script>
+    <!-- responsive tags for screen compatibility -->
+    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+    <!-- custom css --> 
+    <link href="customcss/regiformstyle.css" rel="stylesheet" type="text/css">
+    <!-- bootstrap css --> 
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"> 
+    <!-- fontawesome icons -->
+    <script src="https://kit.fontawesome.com/67a9b7069e.js" crossorigin="anonymous"></script>
+    <script src="bootstrap/js/bootstrap.bundle.js" type="text/javascript"> </script>
+
+
+            <script>
+            $(".toggle-password").click(function() {
+            $(this).toggleClass("fa-eye fa-eye-slash");
+            var input = $($(this).attr("toggle"));
+            if (input.attr("type") == "password") {
+            input.attr("type", "text");
+            } else {
+            input.attr("type", "password");
+            }
+            });
+
+        function calculateAge() {
+        var birthdate = document.getElementById('birthdate').value;
+        var today = new Date();
+        var birthdateObj = new Date(birthdate);
+        var age = today.getFullYear() - birthdateObj.getFullYear();
+
+        // Check if the birthday has occurred this year
+        if (today.getMonth() < birthdateObj.getMonth() || (today.getMonth() === birthdateObj.getMonth() && today.getDate() < birthdateObj.getDate())) {
+            age--;
+        }
+
+        // Update the "Age" input field
+        document.getElementById('age').value = age;
+    }
+        </script>
+
+        <!-- Logout Modal-->
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                        <a class="btn btn-primary" href="logout.php">Logout</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Bootstrap core JavaScript-->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Core plugin JavaScript-->
+        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+        <!-- Custom scripts for all pages-->
+        <script src="js/sb-admin-2.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-modal/2.2.6/js/bootstrap-modalmanager.min.js" integrity="sha512-/HL24m2nmyI2+ccX+dSHphAHqLw60Oj5sK8jf59VWtFWZi9vx7jzoxbZmcBeeTeCUc7z1mTs3LfyXGuBU32t+w==" crossorigin="anonymous"></script>
+        <!-- responsive tags for screen compatibility -->
+        <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+        <!-- bootstrap css --> 
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"> 
+        <!-- fontawesome icons -->
+        <script src="https://kit.fontawesome.com/67a9b7069e.js" crossorigin="anonymous"></script>
+        <script src="bootstrap/js/bootstrap.bundle.js" type="text/javascript"> </script>
+
+    <?php 
+        include('dashboard_sidebar_end.php');
+    ?>
